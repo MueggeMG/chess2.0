ba381fddf97b050e0e30d1848ec73812f7fe5afa
Muegge <marcelgraf79@gmail.com>
2026-04-15 20:38:31 +0200

    fix: lobby = new index.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/chess2.0/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'game.html'),
+      },
+    },
+  },
 });
